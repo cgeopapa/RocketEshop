6a71934829e0769dc83dfc7f49c6b7a33dd3215c
Georgios Papadopoulos <georgios.papadopoulos@eurodyn.com>
2023-03-16 22:44:36 +0200

    Renamed dto to model

---
+++ RocketEshop/Model/Games/Create.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RocketEshop.Model.Games
+{
+    public class Create
+    {
+        public GameViewModel GameViewModel { get; set; }
+        public SelectList genreOptions { get; set; }
+
+        public Create() { }
+
+        public Create(SelectList genreOptions)
+        {
+            this.GameViewModel = new GameViewModel();
+            this.genreOptions = genreOptions;
+        }
+    }
+}
---
+++ RocketEshop/Model/Games/Edit.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using RocketEshop.Core.Models;
+
+namespace RocketEshop.Model.Games
+{
+    public class Edit
+    {
+        public GameViewModel GameViewModel { get; set; }
+        public SelectList genreOptions { get; set; }
+
+        public Edit() { }
+
+        public Edit(Game game, SelectList genreOptions)
+        {
+            this.GameViewModel = new GameViewModel(game);
+            this.genreOptions = genreOptions;
+        }
+    }
+}
