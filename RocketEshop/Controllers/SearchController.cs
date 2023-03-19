ec5042ac8b3612d8c813167cd814244c1c77e26c
Eleni Navrozidou <hnavrozidou@gmail.com>
2023-03-19 12:08:07 +0200

    Created Search controller - did some minor changes

---
+++ RocketEshop/Controllers/SearchController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketEshop.Core.Domain;
+using RocketEshop.Core.Interfaces;
+using RocketEshop.Infrastructure.Data.ViewModel;
+
+namespace RocketEshop.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IGamesService _gamesService;
+        private static Filters Filters;
+
+        public SearchController(IGamesService service)
+        {
+            this._gamesService = service;
+            Filters ??= new Filters();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(new HomeVM(_gamesService.FetchFilteredGamesList(Filters), SearchController.Filters));
+        }
+
+        [HttpPost]
+        [ActionName("Search")]
+        public IActionResult Index([Bind("QuickSearchFilter,Availability,Sorting,MinPrice,MaxPrice")] Filters filtersVm)
+        {
+            Filters = filtersVm;
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
