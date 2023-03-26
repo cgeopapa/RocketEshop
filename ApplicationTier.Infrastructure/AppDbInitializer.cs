392294acb50bfc2e7de91028c3421ddaf1254c4c
Georgios Papadopoulos <georgios.papadopoulos@eurodyn.com>
2023-03-26 17:55:56 +0300

    Program and Seed refactor

--- ApplicationTier.Infrastructure/AppDbInitializer.cs
+++ ApplicationTier.Infrastructure/AppDbInitializer.cs
@@ -8,67 +8,58 @@ namespace RocketEshop.Infrastructure
 {
     public class AppDbInitializer
     {
-        public static void Seed(IApplicationBuilder applicationBuilder)
+        public static async Task Seed(IApplicationBuilder applicationBuilder)
         {
             using IServiceScope serviceScope = applicationBuilder.ApplicationServices.CreateScope();
             AppDbContext? context = serviceScope.ServiceProvider.GetService<AppDbContext>() ?? throw new Exception("Could not get AppDbContext");
-            
-            context.Database.EnsureCreated();
-            // Remove all from Games table. Do not delete the entire DB. Will be useful later
-            // context.Games.RemoveRange(context.Games);
-            context.Games.AddRange(new List<Game>(){
-                new Game()
-                {
-                    Title = "RISE OF THE TOMB RAIDER 20 YEARS CELEBRATION",
-                    Price = 20,
-                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.00408.jpg",
-                    Quantity = 15,
-                    Rating = Rating.Excellent,
-                    ReleaseDate = new DateTime(2016,10,11),
-                    Description = "Rise of the Tomb Raider is a 2015 action-adventure video game developed by Crystal Dynamics and published by Microsoft Studios and Square Enix's European subsidiary. The game is the eleventh main entry in the Tomb Raider series, the sequel to the 2013's Tomb Raider, and is the second instalment in the Survivor trilogy."
-                },
-                new Game()
-                {
-                    Title = "JUST CAUSE 4",
-                    Price = 25,
-                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.00824.jpg",
-                    Quantity = 10,
-                    Rating = Rating.Good,
-                    ReleaseDate = new DateTime(2018,12,4),
-                    Description = "Just Cause 4 is a 2018 action-adventure game developed by Avalanche Studios and published by Square Enix. It is the fourth game in the Just Cause series and the sequel to 2015's Just Cause 3 and was released for Microsoft Windows, PlayStation 4, and Xbox One on 4 December 2018."
-                },
-                new Game()
-                {
-                    Title = "KILLZONE: SHADOW FALL",
-                    Price = 20,
-                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.00008.jpg",
-                    Quantity = 3,
-                    Rating = Rating.Average,
-                    ReleaseDate = new DateTime(2013,11,15),
-                    Description = "Killzone Shadow Fall is a 2013 first-person shooter video game developed by Guerrilla Games and published by Sony Computer Entertainment for the PlayStation 4. It is the sixth game of the Killzone series and the fourth game of the series for home consoles."
-                },
-                new Game()
-                {
-                    Title = "NIOH",
-                    Price = 40,
-                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.00841.jpg",
-                    Quantity = 0,
-                    Rating = Rating.Average,
-                    ReleaseDate = new DateTime(2017,2,7),
-                    Description = "Nioh is an action role-playing video game developed by Team Ninja. It was released for PlayStation 4 in February 2017, and was published by Sony Interactive Entertainment internationally, and by Koei Tecmo in Japan."
-                },
-                new Game()
-                {
-                    Title = "WASTELAND 3 - DAY ONE EDITION",
-                    Price = 55,
-                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.01647.jpg",
-                    Quantity = 0,
-                    Rating = Rating.Bad,
-                    ReleaseDate = new DateTime(2020,8,28),
-                    Description = "Wasteland 3 is a role-playing video game developed by inXile Entertainment and published by Deep Silver. It is a sequel to Wasteland 2 (2014) and was released for Microsoft Windows, PlayStation 4 and Xbox One on August 28, 2020. It was ported to Linux and macOS on December 17, 2020."
-                }
-            });
+            var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
+            context.Database.EnsureCreated();
+
+            SeedGames(context);
+            SeedGenres(context);
+            
+            await SeedRoles(context, roleManager);
+            await SeedUsers(context, userManager);
+
+            context.SaveChanges();
+        }
+
+        private static async Task SeedUsers(AppDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            context.Users.RemoveRange(context.Users);
+            var newAdminUser = new ApplicationUser()
+            {
+                FullName = "Admin User",
+                UserName = "admin-user",
+                Email = "admin@RocketEshop.com",
+                EmailConfirmed = true
+            };
+            await userManager.CreateAsync(newAdminUser, "Rocket1234!");
+            await userManager.AddToRoleAsync(newAdminUser, UserRole.ADMIN.ToString());
+
+            var newAppUser = new ApplicationUser()
+            {
+                FullName = "App User",
+                UserName = "app-user",
+                Email = "user@RocketEshop.com",
+                EmailConfirmed = true,
+                ShoppingCart = new List<ShoppingCartItem>()
+            };
+            await userManager.CreateAsync(newAppUser, "User1234!");
+            await userManager.AddToRoleAsync(newAppUser, UserRole.USER.ToString());
+        }
+
+        private static async Task SeedRoles(AppDbContext context, RoleManager<IdentityRole> roleManager)
+        {
+            context.Roles.RemoveRange(context.Roles);
+            await roleManager.CreateAsync(new IdentityRole(UserRole.ADMIN.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(UserRole.USER.ToString()));
+        }
+
+        private static void SeedGenres(AppDbContext context)
+        {
             context.Genres.RemoveRange(context.Genres);
             context.Genres.AddRange(new List<Genre>()
             {
@@ -89,57 +80,69 @@ namespace RocketEshop.Infrastructure
                     Name = "Racing"
                 }
             });
-            context.SaveChanges();
         }
-        public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
+
+        private static void SeedGames(AppDbContext context)
         {
-            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            context.Games.RemoveRange(context.Games);
+            context.Games.AddRange(new List<Game>()
             {
-
-                // Roles
-                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-                if (!await roleManager.RoleExistsAsync(UserRole.ADMIN.ToString()))
-                    await roleManager.CreateAsync(new IdentityRole(UserRole.ADMIN.ToString()));
-                if (!await roleManager.RoleExistsAsync(UserRole.USER.ToString()))
-                    await roleManager.CreateAsync(new IdentityRole(UserRole.USER.ToString()));
-
-                // Admin
-                var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-                string adminUserEmail = "admin@RocketEshop.com";
-
-                var adminUser = await userManager.FindByEmailAsync(adminUserEmail);
-                if (adminUser == null)
+                new Game()
                 {
-                    var newAdminUser = new ApplicationUser()
-                    {
-                        FullName = "Admin User",
-                        UserName = "admin-user",
-                        Email = adminUserEmail,
-                        EmailConfirmed = true
-                    };
-                    await userManager.CreateAsync(newAdminUser, "Rocket1234!");
-                    await userManager.AddToRoleAsync(newAdminUser, UserRole.ADMIN.ToString());
-                }
-
-                // Users
-                string appUserEmail = "user@RocketEshop.com";
-
-                var appUser = await userManager.FindByEmailAsync(appUserEmail);
-                if (appUser == null)
+                    Title = "RISE OF THE TOMB RAIDER 20 YEARS CELEBRATION",
+                    Price = 20,
+                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.00408.jpg",
+                    Quantity = 15,
+                    Rating = Rating.Excellent,
+                    ReleaseDate = new DateTime(2016, 10, 11),
+                    Description =
+                        "Rise of the Tomb Raider is a 2015 action-adventure video game developed by Crystal Dynamics and published by Microsoft Studios and Square Enix's European subsidiary. The game is the eleventh main entry in the Tomb Raider series, the sequel to the 2013's Tomb Raider, and is the second instalment in the Survivor trilogy."
+                },
+                new Game()
                 {
-                    var newAppUser = new ApplicationUser()
-                    {
-                        FullName = "App User",
-                        UserName = "app-user",
-                        Email = appUserEmail,
-                        EmailConfirmed = true,
-                        ShoppingCart = new List<ShoppingCartItem>()
-                    };
-                    await userManager.CreateAsync(newAppUser, "User1234!");
-                    await userManager.AddToRoleAsync(newAppUser, UserRole.USER.ToString());
+                    Title = "JUST CAUSE 4",
+                    Price = 25,
+                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.00824.jpg",
+                    Quantity = 10,
+                    Rating = Rating.Good,
+                    ReleaseDate = new DateTime(2018, 12, 4),
+                    Description =
+                        "Just Cause 4 is a 2018 action-adventure game developed by Avalanche Studios and published by Square Enix. It is the fourth game in the Just Cause series and the sequel to 2015's Just Cause 3 and was released for Microsoft Windows, PlayStation 4, and Xbox One on 4 December 2018."
+                },
+                new Game()
+                {
+                    Title = "KILLZONE: SHADOW FALL",
+                    Price = 20,
+                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.00008.jpg",
+                    Quantity = 3,
+                    Rating = Rating.Average,
+                    ReleaseDate = new DateTime(2013, 11, 15),
+                    Description =
+                        "Killzone Shadow Fall is a 2013 first-person shooter video game developed by Guerrilla Games and published by Sony Computer Entertainment for the PlayStation 4. It is the sixth game of the Killzone series and the fourth game of the series for home consoles."
+                },
+                new Game()
+                {
+                    Title = "NIOH",
+                    Price = 40,
+                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.00841.jpg",
+                    Quantity = 0,
+                    Rating = Rating.Average,
+                    ReleaseDate = new DateTime(2017, 2, 7),
+                    Description =
+                        "Nioh is an action role-playing video game developed by Team Ninja. It was released for PlayStation 4 in February 2017, and was published by Sony Interactive Entertainment internationally, and by Koei Tecmo in Japan."
+                },
+                new Game()
+                {
+                    Title = "WASTELAND 3 - DAY ONE EDITION",
+                    Price = 55,
+                    ImageUrl = "https://www.e-shop.gr/images/PS4/PS4.01647.jpg",
+                    Quantity = 0,
+                    Rating = Rating.Bad,
+                    ReleaseDate = new DateTime(2020, 8, 28),
+                    Description =
+                        "Wasteland 3 is a role-playing video game developed by inXile Entertainment and published by Deep Silver. It is a sequel to Wasteland 2 (2014) and was released for Microsoft Windows, PlayStation 4 and Xbox One on August 28, 2020. It was ported to Linux and macOS on December 17, 2020."
                 }
-            }
+            });
         }
     }
 }
--- RocketEshop/Program.cs
+++ RocketEshop/Program.cs
@@ -14,18 +14,72 @@ namespace RocketEshop
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
-            
-            builder.Services.AddTransient<IGamesService, GamesService>();
-            builder.Services.AddTransient<IGenresService, GenresService>();
-            builder.Services.AddTransient<IOrdersService, OrdersService>();
-            builder.Services.AddTransient<IApplicationUserService, ApplicationUserService>();
+            SetUpDbAndRepositories(builder);
+            var localizationOptions = SetUpLocalization(builder);
+            SetUpSession(builder);
+            SetupIdentity(builder);
+            builder.Services.AddControllersWithViews();
 
+            var app = builder.Build();
+            SetUpApp(app, localizationOptions);
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            if (builder.Configuration.GetValue<bool>("InitializeDB"))
+            {
+                await AppDbInitializer.Seed(app);
+            }
+
+            app.Run();
+        }
+
+        private static void SetUpApp(WebApplication app, RequestLocalizationOptions localizationOptions)
+        {
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+            app.UseRequestLocalization(localizationOptions);
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+
+        private static void SetupIdentity(WebApplicationBuilder builder)
+        {
+            // Identity Services
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+            builder.Services.AddMemoryCache();
+            builder.Services.AddSession();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            });
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminOnly", policy => policy.RequireRole(UserRole.ADMIN.ToString()));
+            });
+        }
+
+        private static void SetUpSession(WebApplicationBuilder builder)
+        {
+            // Session
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            builder.Services.AddSession();
+        }
+
+        private static RequestLocalizationOptions SetUpLocalization(WebApplicationBuilder builder)
+        {
             // Set up available localizations
             var supportedCultures = new[]
             {
@@ -42,59 +96,20 @@ namespace RocketEshop
 
             // Where to find translation resources
             builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-
-            builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages().AddViewLocalization();
+            return localizationOptions;
+        }
 
-            // Session
-            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            builder.Services.AddSession();
+        private static void SetUpDbAndRepositories(WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
-            // Identity Services
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
-            builder.Services.AddMemoryCache();
-            builder.Services.AddSession();
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            });
-            builder.Services.AddAuthorization(options =>
-            {
-                options.AddPolicy("AdminOnly", policy => policy.RequireRole(UserRole.ADMIN.ToString()));
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
-
-            // Session
-            app.UseSession();
-
-            app.UseRouting();
-
-            // Use Localization
-            app.UseRequestLocalization(localizationOptions);
-
-            //Authentication & Authorization
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            if (builder.Configuration.GetValue<bool>("InitializeDB"))
-            {
-                AppDbInitializer.Seed(app);
-                AppDbInitializer.SeedUsersAndRolesAsync(app).Wait();
-            }
-
-            app.Run();
+            builder.Services.AddTransient<IGamesService, GamesService>();
+            builder.Services.AddTransient<IGenresService, GenresService>();
+            builder.Services.AddTransient<IOrdersService, OrdersService>();
+            builder.Services.AddTransient<IApplicationUserService, ApplicationUserService>();
         }
     }
 }
